a7805bb0cbc4daf56b7c4c41125a1ed596c33a29
srawanreddy <srawanreddy@gmail.com>
2018-03-19 22:34:41 -0500

    Update PascalsTriangle.java

--- pascalsTrianglewithJunit/PascalsTriangle.java
+++ pascalsTrianglewithJunit/PascalsTriangle.java
@@ -6,13 +6,9 @@ public class PascalsTriangle {
     public  void printPascalsTriangle(int numRows)
     {
          
-      // Iterate through every line
-      // and print entries in it
-      for (int line = 0; line < numRows; line++)
+            for (int line = 0; line < numRows; line++)
       {
-        // Every line has number of 
-        // integers equal to line number
-    	  for(int space=0;space<=numRows-line;space++) System.out.print(" ");
+           	  for(int space=0;space<=numRows-line;space++) System.out.print(" ");
     	  System.out.print("[ ");
         for (int i = 0; i <= line; i++)
           System.out.print(BinominalCoef(line, i)+" ");
@@ -21,8 +17,7 @@ public class PascalsTriangle {
       }
     }
       
-    // Link for details of this function
-    // https://www.geeksforgeeks.org/archives/25621
+    
    public  int BinominalCoef(int n, int k)   //n=1, k=1
     {
         int res = 1;
@@ -38,6 +33,6 @@ public class PascalsTriangle {
         return res;
     }
       
-    // Driver code
+    
    
 }
